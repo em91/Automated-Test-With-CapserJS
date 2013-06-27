e7558f1f8129b8b4155060e7d092d31930f43676
xushaozhen <hzxushaozhen@corp.netease.com>
2013-06-27 12:59:55 +0800

    misc：删除casper.test.begin，转移到新分支开发

--- tests/read.js
+++ tests/read.js
@@ -1,38 +1,36 @@
-casper.test.begin("Test Read Module", 1, function(){
-	//从列表进入读信
-	casper.thenClick( x( $Utils.getMboxNavFolderXpath( $Folder.read.name ) ), function(){
-		casper.waitForSelector({
-			type: "xpath",
-			path: $XPATH.LIST_CONTAINER_EXIST
-		}, function(){
-			$Utils.capture( "normal_list.png" );
-		})
+//从列表进入读信
+casper.thenClick( x( $Utils.getMboxNavFolderXpath( $Folder.read.name ) ), function(){
+	casper.waitForSelector({
+		type: "xpath",
+		path: $XPATH.LIST_CONTAINER_EXIST
+	}, function(){
+		$Utils.capture( "normal_list.png" );
+	})
+})
+
+casper.thenClick( x ( $Utils.getXpathByMid( $MID.link ) ), function(){
+	var readUrl = this.evaluate(function(){
+		return location.protocol + "://" + location.host + "/jy5/xhr/msg/read.do?sid=" + $CONF.sid;
 	})
 
-	casper.thenClick( x ( $Utils.getXpathByMid( $MID.link ) ), function(){
-		var readUrl = this.evaluate(function(){
-			return location.protocol + "://" + location.host + "/jy5/xhr/msg/read.do?sid=" + $CONF.sid;
+	this.waitForResource( function( resource ){
+		if( resource.url.indexOf( "xhr/msg/read.do" ) > -1 ){
+			return true;
+		}
+		return false;
+	}, function(){
+		$Utils.capture( "read.png" );
+
+
+		casper.withFrame('', function(){
+
 		})
-
-		this.waitForResource( function( resource ){
-			if( resource.url.indexOf( "xhr/msg/read.do" ) > -1 ){
-				return true;
-			}
-			return false;
-		}, function(){
-			$Utils.capture( "read.png" );
+	}, function(){
+		this.test.error( "no read.do request." );
+	})
+});
 
 
-			casper.withFrame('', function(){
-
-			})
-		}, function(){
-			this.test.error( "no read.do request." );
-		})
-	});
-
-
-	casper.run(function(){
-		this.test.renderResults( true, 0, this.cli.get( "o" ) || false );
-	});
-})
+casper.run(function(){
+	this.test.renderResults( true, 0, this.cli.get( "o" ) || false );
+});
